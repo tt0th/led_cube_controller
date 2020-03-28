2aab0ecf6f3c44cfa41d95c5aa66063e6bbbde1f
Tibor Toth <toth.t11@gmail.com>
2020-03-28 21:55:01 +0100

    circle animation: change axis and clean prev point

--- src/led_cube/animations/circle.py
+++ src/led_cube/animations/circle.py
@@ -1,7 +1,7 @@
 import math
 import time
 import numpy as np
-from transforms3d.derivations.eulerangles import x_rotation
+from transforms3d.derivations.angle_axes import angle_axis2mat
 from .animation import Animation
 from ..led_cube_controller import LedCubeController
 
@@ -9,10 +9,13 @@ from ..led_cube_controller import LedCubeController
 class CircleAnimation(Animation):
     def __init__(self):
         super().__init__()
-        self.point = np.array([2, 2, 2])
+        self.point = np.array([4, 4, 4])
 
     def animate(self, controller: LedCubeController):
-        rotation_matrix = np.array(x_rotation(math.pi / 8))
+        rotation_matrix = np.array(angle_axis2mat(math.pi / 8), [1, 1, 1])
+
+        controller.turn_off(round(self.point[0]), round(self.point[1]), round(self.point[2]))
         self.point = self.point.dot(rotation_matrix)
         controller.turn_on(round(self.point[0]), round(self.point[1]), round(self.point[2]))
+
         time.sleep(0.5)
